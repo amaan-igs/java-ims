6edcc1071e19953b6534b97eda342df36592a4d7
Amaan Ul Haq Siddiqui <amaanulhaq55@gmail.com>
2024-06-12 15:21:00 +0500

    Create-DB

---
+++ db/setup.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE inventorydb;
+
+USE inventorydb;
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
